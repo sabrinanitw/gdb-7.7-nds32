3b1fc36d88452cf6422c62473220220e0df07f1b
Sabrina Ni <sabrina@andestech.com>
2013-10-30 14:08:32 +0800

    (nds32) extend file-I/O protocol. warning fixed by kai.

--- gdb/remote-fileio.c
+++ gdb/remote-fileio.c
@@ -842,7 +842,7 @@ remote_fileio_func_write (char *buf)
       return;
     }
   length = (size_t) num;
-    
+
   buffer = (gdb_byte *) xmalloc (length);
   if (target_read_memory (ptrval, buffer, length) != 0)
     {
@@ -924,7 +924,7 @@ remote_fileio_func_lseek (char *buf)
       remote_fileio_reply (-1, FILEIO_EINVAL);
       return;
     }
-  
+
   remote_fio_no_longjmp = 1;
   ret = lseek (fd, offset, flag);
 
@@ -949,14 +949,14 @@ remote_fileio_func_rename (char *buf)
       remote_fileio_ioerror ();
       return;
     }
-  
+
   /* 2. Parameter: Ptr to newpath / length incl. trailing zero */
   if (remote_fileio_extract_ptr_w_len (&buf, &new_ptr, &new_len))
     {
       remote_fileio_ioerror ();
       return;
     }
-  
+
   /* Request oldpath using 'm' packet */
   oldpath = alloca (old_len);
   if (target_read_memory (old_ptr, (gdb_byte *) oldpath, old_len) != 0)
@@ -964,7 +964,7 @@ remote_fileio_func_rename (char *buf)
       remote_fileio_ioerror ();
       return;
     }
-  
+
   /* Request newpath using 'm' packet */
   newpath = alloca (new_len);
   if (target_read_memory (new_ptr, (gdb_byte *) newpath, new_len) != 0)
@@ -972,7 +972,7 @@ remote_fileio_func_rename (char *buf)
       remote_fileio_ioerror ();
       return;
     }
-  
+
   /* Only operate on regular files and directories.  */
   of = stat (oldpath, &ost);
   nf = stat (newpath, &nst);
@@ -1092,7 +1092,7 @@ remote_fileio_func_stat (char *buf)
       return;
     }
   statptr = (CORE_ADDR) lnum;
-  
+
   /* Request pathname using 'm' packet */
   pathname = alloca (namelength);
   if (target_read_memory (nameptr, (gdb_byte *) pathname, namelength) != 0)
@@ -1302,7 +1302,7 @@ remote_fileio_func_system (char *buf)
 	  return;
 	}
     }
-  
+
   /* Check if system(3) has been explicitely allowed using the
      `set remote system-call-allowed 1' command.  If length is 0,
      indicating a NULL parameter to the system call, return zero to
@@ -1656,7 +1656,7 @@ remote_fileio_func_fgets (char *buf)
   CORE_ADDR ptrval;
   int size;
   FILE *stream;
-  char *buffer;
+  gdb_byte *buffer;
   int retval = 0;
 
   /* 1. Parameter: buffer pointer */
@@ -1683,9 +1683,9 @@ remote_fileio_func_fgets (char *buf)
     }
   stream = (FILE *)stream_int;
 
-  buffer = (gdb_byte *) xmalloc (size);
+  buffer = xmalloc (size);
   remote_fio_no_longjmp = 1;
-  if (fgets (buffer, size, stream) != NULL)
+  if (fgets ((char *)buffer, size, stream) != NULL)
     {
       errno = target_write_memory (ptrval, buffer, size);
       if (errno != 0)
@@ -1743,7 +1743,7 @@ remote_fileio_func_fputs (char *buf)
   CORE_ADDR ptrval;
   FILE *stream;
   int retval;
-  char buffer[2048]; /* TODO: Assume the maximum length of string is 2048.  */
+  gdb_byte buffer[2048]; /* TODO: Assume the maximum length of string is 2048.  */
 
   /* 1. Parameter: buffer pointer */
   if (remote_fileio_extract_int (&buf, &lnum))
@@ -1768,7 +1768,7 @@ remote_fileio_func_fputs (char *buf)
     }
 
   remote_fio_no_longjmp = 1;
-  retval = fputs (buffer, stream);
+  retval = fputs ((const char *)buffer, stream);
   if (retval == EOF)
     remote_fileio_return_errno (-1);
   else
